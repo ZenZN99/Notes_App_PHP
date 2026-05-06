dddbe404348f97ef1baa266427a6fc59a2450a94
Zen Allaham <hxfhfucicic@gmail.com>
2026-05-07 00:31:13 +0300

    Update edit_note.php

--- edit_note.php
+++ edit_note.php
@@ -9,8 +9,8 @@ if (!isset($_SESSION['user_id'])) {
 
 $id = $_GET['id'];
 
-$stmt = $conn->prepare("SELECT * FROM notes WHERE id = ?");
-$stmt->bind_param("i", $id);
+$stmt = $conn->prepare("SELECT * FROM notes WHERE id = ? AND user_id = ?");
+$stmt->bind_param("ii", $id, $_SESSION['user_id']);
 $stmt->execute();
 $note = $stmt->get_result()->fetch_assoc();
 
