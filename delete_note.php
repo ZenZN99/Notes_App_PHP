76626da0cfddb2a547b404972fcfdc2ca8f8e9fe
Zen Allaham <hxfhfucicic@gmail.com>
2026-05-07 00:31:29 +0300

    Update delete_note.php

--- delete_note.php
+++ delete_note.php
@@ -9,8 +9,8 @@ if (!isset($_SESSION['user_id'])) {
 
 $id = $_GET['id'];
 
-$stmt = $conn->prepare("DELETE FROM notes WHERE id = ?");
-$stmt->bind_param("i", $id);
+$stmt = $conn->prepare("SELECT * FROM notes WHERE id = ? AND user_id = ?");
+$stmt->bind_param("ii", $id, $_SESSION['user_id']);
 $stmt->execute();
 
 header("Location: notes.php");
